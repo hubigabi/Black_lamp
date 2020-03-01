3e75e14d5b750380f2bc60522e5fb47e5f70296f
hubigabi <hubigabi19@gmail.com>
2020-03-01 20:08:12 +0100

    Added skeleton of the game

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    private float speed = 50.0f;
+    private int direction = 0;
+
+    private float leftBorder;
+    private float rightBorder;
+
+    public void setDirection(int value) {
+        direction = value;
+    }
+
+    void Start()
+    {
+        leftBorder = GameObject.Find("LeftBorder").transform.position.x;
+        rightBorder = GameObject.Find("RightBorder").transform.position.x;
+    }
+
+
+    void Update()
+    {
+        transform.position += new Vector3(direction, 0, 0) * speed * Time.deltaTime;
+
+        if (transform.position.x < leftBorder || transform.position.x > rightBorder) {
+            Destroy(gameObject);
+        }
+    
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+        var move = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, 0);
+
+        if ((CrossPlatformInputManager.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W)) && Mathf.Abs(rb.velocity.y) < 1.0) {
+            rb.AddForce(Vector2.up * 2500);
+        }
+
+        transform.position += move * speed * Time.deltaTime;
+
+        //For keyboard
+        var moveForKeyboard = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += moveForKeyboard * speed * Time.deltaTime;
+
+        if (CrossPlatformInputManager.GetButtonDown("LeftShoot") || Input.GetKeyDown(KeyCode.K)) {
+            FireBall newFireBall = Instantiate(GameObject.FindGameObjectsWithTag("FireBall")[0].GetComponent<FireBall>(), 
+                new Vector3(rb.position.x, rb.position.y + 5, 0.0f), Quaternion.identity);
+
+            newFireBall.setDirection(-1);
+        } else if (CrossPlatformInputManager.GetButtonDown("RightShoot") || Input.GetKeyDown(KeyCode.L)) {
+            FireBall newFireBall = Instantiate(GameObject.FindGameObjectsWithTag("FireBall")[0].GetComponent<FireBall>(),
+                new Vector3(rb.position.x, rb.position.y + 5, 0.0f), Quaternion.identity);
+
+            newFireBall.setDirection(1);
+        }
+
+    }
+}
