679335b9a0db81f0995e3cba0de6b24af32ed568
Szymon1109 <szymonbetlewski@wp.pl>
2020-03-24 14:45:00 +0100

    Added menu scene

---
+++ Assets/Scripts/LevelsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel1()
+    {
+       SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+}
